185e3344ae98d01a5913b7c7392cab9bf89d7ac4
MafSholl <marvesholl1@gmail.com>
2022-11-18 12:28:34 +0100

    first commit-project

---
+++ src/main/java/com/lakesidehotel/LakesideHotelApplication.java
@@ -0,0 +1,13 @@
+package com.lakesidehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakesideHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakesideHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lakesidehotel/LakesideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakesidehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakesideHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
