303f859efaa6ac400cb766c32e57362199a89c6a
MafSholl <marvesholl1@gmail.com>
2022-11-20 00:48:26 +0100

    refined global exception

--- src/main/java/com/lakesidehotel/app/exceptions/exceptionClasses/LakesideHotelException.java
+++ src/main/java/com/lakesidehotel/app/exceptions/exceptionClasses/LakesideHotelException.java
@@ -1,11 +1,16 @@
 package com.lakesidehotel.app.exceptions.exceptionClasses;
 
+import lombok.Getter;
+
 public class LakesideHotelException extends RuntimeException {
+    @Getter
+    private int statusCode;
     public LakesideHotelException(String message) {
         super(message);
     }
 
     public LakesideHotelException(String message, int statusCode) {
         super(message);
+        this.statusCode = statusCode;
     }
 }
